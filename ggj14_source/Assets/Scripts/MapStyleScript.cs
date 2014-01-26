3a190b95a1f031e5108add4348544586f3c7d48f
gregoriobenatti <gregoriobenatti@gmail.com>
2014-01-26 17:48:34 -0200

    beta2 - changing map color

--- ggj14_source/Assets/Scripts/MapStyleScript.cs
+++ ggj14_source/Assets/Scripts/MapStyleScript.cs
@@ -2,26 +2,30 @@
 using System.Collections;
 
 public class MapStyleScript : MonoBehaviour {
-	//public Texture backbroundGreen;
-	//public Texture backbroundBrown;
-	//public Texture backbroundYellow;
+	public SpriteRenderer renderer;
+	public Sprite spriteGreen;
+	public Sprite spriteYellow;
+	public Sprite spriteBrown;
 
-	//public GameObject mySprite;
 
 	void Start () {
 	
 	}
 	
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.W)){
-			//renderer.material.(0, backbroundYellow);
-			//mySprite.renderer.material.SetTexture(0, backbroundGreen);
-		}
 		if (Input.GetKeyDown(KeyCode.Q)){
-			//mySprite. material.mainTexture=(0, backbroundYellow);
+			renderer = gameObject.GetComponent<SpriteRenderer>();
+			renderer.sprite = spriteGreen;
 		}
+
+		if (Input.GetKeyDown(KeyCode.W)){
+			renderer = gameObject.GetComponent<SpriteRenderer>();
+			renderer.sprite = spriteYellow;
+		}
+
 		if (Input.GetKeyDown(KeyCode.E)){
-			//mySprite.renderer.material.SetTexture(0, backbroundBrown);
+			renderer = gameObject.GetComponent<SpriteRenderer>();
+			renderer.sprite = spriteBrown;
 		}
 	}
 }
